52713153c37942c208972869beceedbc0b1d5151
Nope <nothingmore2003@gmail.com>
2024-04-09 23:36:40 +0700

    init project

---
+++ src/training/HelloWorld.java
@@ -0,0 +1,11 @@
+package training;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with java code");
+		System.out.println("Hello World with java code");
+		System.out.println("Hello World with java code");
+		System.out.println("Hello World with java code");
+
+	}
+}
